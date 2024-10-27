057f366b1261a5460b744c1c0460c3c916b04ce7
FalconEagleTest <34710362+FalconEagleTest@users.noreply.github.com>
2024-10-27 07:06:17 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ fn encrypt_ige<'a>(plain: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a m
     let cipher = grammers_crypto::encrypt_ige(plain, &key_array, &iv_array);
    
     match String_result::from_utf8(cipher) {
-        Ok(string) => return String_result,
+        Ok(String_result) => return String_result,
         Err(e) => return"Error: {e}",
     }
     
@@ -19,7 +19,7 @@ fn decrypt_ige<'a>(cipher: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a 
 
     let plain = grammers_crypto::decrypt_ige(cipher, &key_array, &iv_array);
     match String_result::from_utf8(plain) {
-        Ok(string) => return String_result,
+        Ok(String_result) => return String_result,
         Err(e) => return "Error: {e}",
     }
 
