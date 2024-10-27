43ab590f8c58790b2b26c76e29e86461a204002b
FalconEagleTest <34710362+FalconEagleTest@users.noreply.github.com>
2024-10-27 09:30:19 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,9 @@ fn encrypt_ige<'a>(plain: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a m
 
     let cipher = grammers_crypto::encrypt_ige(plain, &key_array, &iv_array);
     let s = format!("{:?}", &cipher);
-    let mut buffer = [0; 1024];
-    let mut test: &mut[u8] = &mut buffer;
-    test.write(s.as_bytes()).unwrap();
     
-    return &test;
+    
+    return s.as_bytes();
     
 
 
@@ -20,11 +18,9 @@ fn decrypt_ige<'a>(cipher: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a 
 
     let plain = grammers_crypto::decrypt_ige(cipher, &key_array, &iv_array);
     let s = format!("{:?}", &plain);
-    let mut buffer = [0; 1024];
-    let mut test: &mut[u8] = &mut buffer;
-    test.write(s.as_bytes()).unwrap();
     
-    return &test;
+    
+    return s.as_bytes();
 
 }
 
