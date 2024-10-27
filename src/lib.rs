d7df28676d0efbae5de1778bd16142a651050928
FalconEagleTest <34710362+FalconEagleTest@users.noreply.github.com>
2024-10-27 08:51:18 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 /// Encrypts the input plain text with the 32 bytes key and IV.
 #[no_mangle]
-fn encrypt_ige<'a>(plain: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a mut [u8; 32]) -> Vec[u8]  {
+fn encrypt_ige<'a>(plain: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a mut [u8; 32]) -> Vec<u8>  {
 
     let cipher = grammers_crypto::encrypt_ige(plain, &key_array, &iv_array);
    
@@ -12,7 +12,7 @@ fn encrypt_ige<'a>(plain: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a m
 
 /// Decrypts the input cipher text with the 32 bytes key and IV.
 #[no_mangle]
-fn decrypt_ige<'a>(cipher: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a mut [u8; 32]) -> Vec[u8]   {
+fn decrypt_ige<'a>(cipher: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a mut [u8; 32]) -> Vec<u8>   {
 
     let plain = grammers_crypto::decrypt_ige(cipher, &key_array, &iv_array);
    
