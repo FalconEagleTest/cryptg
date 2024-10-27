f9ddb2b91ba86493cac7c9035a5e10795570b051
FalconEagleTest <34710362+FalconEagleTest@users.noreply.github.com>
2024-10-27 08:36:30 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 /// Encrypts the input plain text with the 32 bytes key and IV.
 #[no_mangle]
-fn encrypt_ige<'a>(plain: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a mut [u8; 32]) ->  String  {
+fn encrypt_ige<'a>(plain: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a mut [u8; 32]) ->  &'a [u8]  {
 
     let cipher = grammers_crypto::encrypt_ige(plain, &key_array, &iv_array);
-    
-    return cipher.try_into().unwrap();
+    let a: [u8; cipher.len()] = v.try_into().unwrap();
+    return a;
     
 
 
@@ -12,11 +12,11 @@ fn encrypt_ige<'a>(plain: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a m
 
 /// Decrypts the input cipher text with the 32 bytes key and IV.
 #[no_mangle]
-fn decrypt_ige<'a>(cipher: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a mut [u8; 32]) -> String  {
+fn decrypt_ige<'a>(cipher: &'a [u8], key_array: &'a mut [u8; 32], iv_array: &'a mut [u8; 32]) -> &'a [u8]   {
 
     let plain = grammers_crypto::decrypt_ige(cipher, &key_array, &iv_array);
-    
-    return plain.try_into().unwrap();
+    let a: [u8; plain.len()] = v.try_into().unwrap();
+    return a;
 
 }
 
